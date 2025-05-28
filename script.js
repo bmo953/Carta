bb8f9e0cbd13a6bbecbdc106bbb063bb31f6dbb7
bmo953 <brayanjosue2001@gmail.com>
2025-05-28 15:39:16 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const envoltura = document.querySelector(".envoltura-sobre");
+const carta = document.querySelector(".carta");
+
+document.addEventListener("click", (e) => {
+    if (e.target.matches(".sobre") || 
+        e.target.matches(".solapa-derecha") ||
+        e.target.matches(".solapa-izquierda") ||
+        e.target.matches(".corazon")) {
+        envoltura.classList.toggle("abierto");
+      
+    } else if (e.target.matches(".sobre *")) {
+        if (!carta.classList.contains("abierta")) {
+            carta.classList.add("mostrar-carta");
+
+            setTimeout(() => {
+                carta.classList.remove("mostrar-carta");
+                carta.classList.add("abierta");
+            }, 500);
+            envoltura.classList.add("desactivar-sobre")
+        } else {
+            carta.classList.add("cerrando-carta");
+            envoltura.classList.remove("desactivar-sobre");
+
+            setTimeout(() => {
+                carta.classList.remove("cerrando-carta")
+                carta.classList.remove("abierta")
+            }, 500);
+        }
+
+    } 
+})
+// EFECTO: corazones flotantes al mover el mouse
+document.addEventListener("mousemove", function(e) {
+    const heart = document.createElement("div");
+    heart.classList.add("floating-heart");
+    heart.style.left = `${e.clientX}px`;
+    heart.style.top = `${e.clientY}px`;
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 1000);
+});
